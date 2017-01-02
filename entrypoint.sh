a57e62b15643620650e14c3787961bce85ff8213
Iku Iwasa <iku.iwasa@gmail.com>
2017-01-02 21:30:29 +0900

    Fetch (M)ELPA packages only if EMACS_PACKAGES is specified

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ EMACS_DIR=$HOME/.emacs.d/
 EMACS_ELPA_DIR=$EMACS_DIR/elpa/
 EMACS_INIT_FILE=$EMACS_DIR/init.el
 
-if [ "$1" = emacs -a ! -d "$EMACS_ELPA_DIR" ]; then
+if [ "$1" = emacs -a ! -d "$EMACS_ELPA_DIR" -a -n "$EMACS_PACKAGES" ]; then
 	cat > /tmp/melpa.el <<EOF
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
